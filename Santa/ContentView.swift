410cc9ebbb4ff4948188129b7ede849cbde2d2c4
BIll Ting <bting@lcs.on.ca>
2020-12-03 08:53:36 -0500

    Initial Commit

---
+++ Santa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Santa
+//
+//  Created by Bill Ting on 2020-12-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Santa/SantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SantaApp.swift
+//  Santa
+//
+//  Created by Bill Ting on 2020-12-03.
+//
+
+import SwiftUI
+
+@main
+struct SantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
